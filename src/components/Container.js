cdb1fec33097f4bd7f019634749e58fbdd7c3811
JooNyeongLee <wnsud5959@gmail.com>
2020-03-08 18:33:24 +0900

    200308

---
+++ src/components/Container.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Presentation from './Presentation';
+
+class Container extends Component {
+    state ={
+        count:0,
+    }
+    add = () => {
+        console.log(this.state.count);
+
+        
+
+        var newState = {...this.state};
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+    
+    render() {
+        return(
+            <div>
+                <button onClick={this.add}>+</button>
+                <Presentation>현재 클릭 수 : {this.state.count}</Presentation>
+            </div>
+        );
+    }
+}
+
+export default Container;
